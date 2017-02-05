347f6af39d2b0a28e918ad791b5ded2fb64ad392
Ola <althienao@gmail.com>
2017-02-05 16:29:24 +0100

    version before re-writing

--- Szachy.java
+++ Szachy.java
@@ -5,9 +5,23 @@ public class Szachy
 {
 	public static void main(String args[])
 	{
-		Chessboard chessboard = new Chessboard();
+		Game game = new Game();
 		System.out.println("Szachownica: ");
-		System.out.println(chessboard);
+		System.out.println(game.chessboard());
+		System.out.println("Białe: " + game.white() + ", czarne " + game.black());
+
+		for(int i = 0; i < 5; i++)
+		{
+			System.out.println("Tura " + (i + 1));
+			
+			//white moves
+			game.nextMove();
+			System.out.println(game.chessboard());
+			
+			//black moves
+			game.nextMove();
+			System.out.println(game.chessboard());
+		}
 	}
 	
 }
